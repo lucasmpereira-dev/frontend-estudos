761bf887ca383739a741b38c430157fb225bab4f
Lucas M Pereira <lumape2008@gmail.com>
2026-04-27 23:23:46 +0000

    commit

---
+++ Aulas/introdução/ introducao.js
@@ -0,0 +1,8 @@
+console.log('Hello World!');
+
+//criar uma variavel
+let nome = "Lucas";
+console.log('Seu nome é: '+ nome);
+
+// exibir um alerta no navegador
+alert = ("Bem vindo(a), !" + nome);
